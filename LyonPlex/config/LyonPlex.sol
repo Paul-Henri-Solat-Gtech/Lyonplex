4fe25d927abb1765a48f4d8390ee9d603ca2301f
Paulhenrisolat <psolat@gaming.tech>
2025-05-21 19:22:18 +0200

    Lib & SampleProj [Creation]

---
+++ LyonPlex/config/LyonPlex.sol
@@ -0,0 +1,11 @@
+{
+    "format_version": "12.00",
+    "minimum_version": "10.0.40219.1",
+    "projects": [
+        "SampleProject.prj",
+        "LyonPlexLib.prj"
+    ],
+    "solution_name": "LyonPlex",
+    "version": "16",
+    "version_full": "16.0.30319.14"
+}

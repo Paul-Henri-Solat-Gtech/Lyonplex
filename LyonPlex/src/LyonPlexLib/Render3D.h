93e80db81ee77469f9c03c65ad932e9b649cfcfd
Clement-Trt <ctouratier@gaming.tech>
2025-05-30 12:53:45 +0200

    small clean

--- LyonPlex/src/LyonPlexLib/Render3D.h
+++ LyonPlex/src/LyonPlexLib/Render3D.h
@@ -17,17 +17,13 @@ public:
 	void Release();
 
 private:
+	HWND m_windowWP;
+
 	GraphicsDevice* mp_graphicsDevice;
 	DescriptorManager* mp_descriptorManager;
 	CommandManager* mp_commandManager;
-	HWND m_windowWP;
 
 	GraphicsPipeline m_graphicsPipeline;
 	MeshManager m_meshManager;
-
-
-	// mesh
-	ComPtr<ID3D12Resource> m_vertexBuffer;				// Buffer contenant les sommets à dessiner
-	D3D12_VERTEX_BUFFER_VIEW m_vertexBufferView;		// Vue permettant d’accéder au vertex buffer dans le pipeline
 };
 

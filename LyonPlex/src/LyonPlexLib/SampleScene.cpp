8e0e8bb68c7006d30117cfe802db4b49bdf22d17
Paulhenrisolat <psolat@gaming.tech>
2025-06-03 21:11:11 +0200

    Update SampleScene.cpp

--- LyonPlex/src/LyonPlexLib/SampleScene.cpp
+++ LyonPlex/src/LyonPlexLib/SampleScene.cpp
@@ -6,10 +6,10 @@ void SampleScene::Start()
 	OutputDebugStringA("\nHI ! I am sample scene. \n");
 
 	Entity eTriangle = mp_EcsManager->CreateEntity();
-	mp_EcsManager->AddComponent<MeshComponent>(eTriangle, new MeshComponent(1, 0));
+	mp_EcsManager->AddComponent<MeshComponent>(eTriangle, new MeshComponent(0, 0));
 
 	Entity eSquare = mp_EcsManager->CreateEntity();
-	mp_EcsManager->AddComponent<MeshComponent>(eSquare, new MeshComponent(0, 0));
+	mp_EcsManager->AddComponent<MeshComponent>(eSquare, new MeshComponent(1, 0));
 
 	Entity eCube = mp_EcsManager->CreateEntity();
 	mp_EcsManager->AddComponent<MeshComponent>(eCube, new MeshComponent(2, 0));

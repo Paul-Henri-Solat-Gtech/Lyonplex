17f1d88bc5541b116677add78b0701ac59edd299
Paulhenrisolat <psolat@gaming.tech>
2025-06-04 18:20:49 +0200

    entity & scene Update

--- LyonPlex/src/LyonPlexLib/SampleScene.cpp
+++ LyonPlex/src/LyonPlexLib/SampleScene.cpp
@@ -7,54 +7,64 @@ void SampleScene::Start()
     
    Entity camEntity = mp_EcsManager->CreateEntity();
    mp_EcsManager->AddComponent<CameraComponent>(camEntity, new CameraComponent());
+   //mp_EcsManager->AddComponent<TransformComponent>(camEntity, new TransformComponent());
 
+   AddEntityToScene(camEntity, "camera");
 
-    Entity eSquare = mp_EcsManager->CreateEntity();
+   Entity eSquare = mp_EcsManager->CreateEntity();
    mp_EcsManager->AddComponent<MeshComponent>(eSquare, new MeshComponent(1, 0));
    mp_EcsManager->AddComponent<TransformComponent>(eSquare, new TransformComponent());
    mp_EcsManager->GetComponent<TransformComponent>(eSquare)->position = { -0.60, 0.4, 0.2 };
+   //mp_EcsManager->GetComponent<TransformComponent>(eSquare)->position.x += 0.9f;
    mp_EcsManager->GetComponent<TransformComponent>(eSquare)->scale = { 2, 2, 1 };
+   AddEntityToScene(eSquare, "square");
 
-    /*Entity eSquare2 =mp_EcsManager->CreateEntity();
-   mp_EcsManager->AddComponent<MeshComponent>(eSquare2, new MeshComponent(1, 0));
-   mp_EcsManager->AddComponent<TransformComponent>(eSquare2, new TransformComponent());
-   mp_EcsManager->GetComponent<TransformComponent>(eSquare2)->position = { -0.20, 0.4, 0.2 };*/
-
-    Entity eTriangle = mp_EcsManager->CreateEntity();
+   Entity eTriangle = mp_EcsManager->CreateEntity();
    mp_EcsManager->AddComponent<MeshComponent>(eTriangle, new MeshComponent(0, 0));
    mp_EcsManager->AddComponent<TransformComponent>(eTriangle, new TransformComponent());
    mp_EcsManager->GetComponent<TransformComponent>(eTriangle)->position = { 0.60, 0.4, 0.2 };
    mp_EcsManager->GetComponent<TransformComponent>(eTriangle)->scale = { 3, 3, 1 };
-    /*Entity eTriangle2 =mp_EcsManager->CreateEntity();
-   mp_EcsManager->AddComponent<MeshComponent>(eTriangle2, new MeshComponent(0, 0));
-   mp_EcsManager->AddComponent<TransformComponent>(eTriangle2, new TransformComponent());
-   mp_EcsManager->GetComponent<TransformComponent>(eTriangle2)->position = { -0.20, -0.4, 0.2 };*/
+   AddEntityToScene(eTriangle, "triangle");
 
-    Entity eCube = mp_EcsManager->CreateEntity();
+   Entity eCube = mp_EcsManager->CreateEntity();
    mp_EcsManager->AddComponent<MeshComponent>(eCube, new MeshComponent(2, 0));
    mp_EcsManager->AddComponent<TransformComponent>(eCube, new TransformComponent());
    mp_EcsManager->GetComponent<TransformComponent>(eCube)->position = { 0.2, -0.6, 0 };
+   AddEntityToScene(eCube, "cube");
 
     /*Entity eCube2 =mp_EcsManager->CreateEntity();
    mp_EcsManager->AddComponent<MeshComponent>(eCube2, new MeshComponent(2, 0));
    mp_EcsManager->AddComponent<TransformComponent>(eCube2, new TransformComponent());
    mp_EcsManager->GetComponent<TransformComponent>(eCube2)->position = { -0.4, 0, 0 };*/
     /*mp_EcsManager->GetComponent<TransformComponent>(eSquare)->position = { 0, 0.5, 2 };*/ // IL FAUT FAIRE UNE FONTION QUI APPEL TJRS dirty = true APRES CHAQUE MODIFICATION, SINON LE TRANSFORM EST PAS UPDATE
-
-	/*Entity eTriangle = mp_EcsManager->CreateEntity();
-	mp_EcsManager->AddComponent<MeshComponent>(eTriangle, new MeshComponent(0, 0));
-
-	Entity eSquare = mp_EcsManager->CreateEntity();
-	mp_EcsManager->AddComponent<MeshComponent>(eSquare, new MeshComponent(1, 0));
-
-	Entity eCube = mp_EcsManager->CreateEntity();
-	mp_EcsManager->AddComponent<MeshComponent>(eCube, new MeshComponent(2, 0));*/
 }
 
 void SampleScene::Update()
 {
+    //Input
+    if (InputManager::GetKeyIsPressed('Z'))
+    {
+        //OutputDebugStringA("\nZ is pressed ! \n");
+        mp_EcsManager->GetComponent<TransformComponent>(*GetEntity("cube"))->position.z += 0.01f;
+    }
+    if (InputManager::GetKeyIsPressed('S'))
+    {
+        //OutputDebugStringA("\nZ is pressed ! \n");
+        mp_EcsManager->GetComponent<TransformComponent>(*GetEntity("cube"))->position.z -= 0.01f;
+    }
+    if (InputManager::GetKeyIsPressed('Q'))
+    {
+        //OutputDebugStringA("\nZ is pressed ! \n");
+        mp_EcsManager->GetComponent<TransformComponent>(*GetEntity("cube"))->position.x -= 0.01f;
+    }
+    if (InputManager::GetKeyIsPressed('D'))
+    {
+        //OutputDebugStringA("\nZ is pressed ! \n");
+        mp_EcsManager->GetComponent<TransformComponent>(*GetEntity("cube"))->position.x += 0.01f;
+    }
 }
 
 void SampleScene::Release()
 {
+
 }
--- LyonPlex/src/LyonPlexLib/Scene.h
+++ LyonPlex/src/LyonPlexLib/Scene.h
@@ -2,6 +2,12 @@
 
 #include "SceneManager.h"
 
+struct SceneEntity 
+{
+	Entity entity;
+	std::string name;
+};
+
 class Scene
 {
 public:
@@ -12,10 +18,15 @@ public:
 	virtual void Release();
 
 	void SetEcsManager(ECSManager* ecsManager) { mp_EcsManager = ecsManager; };
+	void AddEntityToScene(Entity entity, const std::string& entityName); // Only entities who need to get update like player or camera, not background or trees ..
+
+	Entity* GetEntity(const std::string& entityName);
 
 protected:
 
 	ECSManager* mp_EcsManager;
 	//std::string m_SceneName;
+
+	std::vector<SceneEntity> m_sceneEntities;
 };
 

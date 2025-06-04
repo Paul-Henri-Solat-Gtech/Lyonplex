b4c8369486d87a7acc66e7108880253e9f008d1a
Paulhenrisolat <psolat@gaming.tech>
2025-06-04 12:27:34 +0200

    put new ecs in scene

--- LyonPlex/src/LyonPlexLib/GameManager.cpp
+++ LyonPlex/src/LyonPlexLib/GameManager.cpp
@@ -43,42 +43,7 @@ bool GameManager::Init()
 
 int GameManager::Run() 
 {
-    Entity camEntity = m_ECS.CreateEntity();
-    m_ECS.AddComponent<CameraComponent>(camEntity, new CameraComponent());
-
-
-    Entity eSquare = m_ECS.CreateEntity();
-    m_ECS.AddComponent<MeshComponent>(eSquare, new MeshComponent(1, 0));
-    m_ECS.AddComponent<TransformComponent>(eSquare, new TransformComponent());
-    m_ECS.GetComponent<TransformComponent>(eSquare)->position = { -0.60, 0.4, 0.2 };
-    m_ECS.GetComponent<TransformComponent>(eSquare)->scale = { 2, 2, 1 };
-
-    /*Entity eSquare2 = m_ECS.CreateEntity();
-    m_ECS.AddComponent<MeshComponent>(eSquare2, new MeshComponent(1, 0));
-    m_ECS.AddComponent<TransformComponent>(eSquare2, new TransformComponent());
-    m_ECS.GetComponent<TransformComponent>(eSquare2)->position = { -0.20, 0.4, 0.2 };*/
-    
-    Entity eTriangle = m_ECS.CreateEntity();
-    m_ECS.AddComponent<MeshComponent>(eTriangle, new MeshComponent(0, 0));
-    m_ECS.AddComponent<TransformComponent>(eTriangle, new TransformComponent());
-    m_ECS.GetComponent<TransformComponent>(eTriangle)->position = { 0.60, 0.4, 0.2 };
-    m_ECS.GetComponent<TransformComponent>(eTriangle)->scale = { 3, 3, 1 };
-    /*Entity eTriangle2 = m_ECS.CreateEntity();
-    m_ECS.AddComponent<MeshComponent>(eTriangle2, new MeshComponent(0, 0));
-    m_ECS.AddComponent<TransformComponent>(eTriangle2, new TransformComponent());
-    m_ECS.GetComponent<TransformComponent>(eTriangle2)->position = { -0.20, -0.4, 0.2 };*/
-
-    Entity eCube = m_ECS.CreateEntity();
-    m_ECS.AddComponent<MeshComponent>(eCube, new MeshComponent(2, 0));
-    m_ECS.AddComponent<TransformComponent>(eCube, new TransformComponent());
-    m_ECS.GetComponent<TransformComponent>(eCube)->position = { 0.2, -0.6, 0 };
-    
-    /*Entity eCube2 = m_ECS.CreateEntity();
-    m_ECS.AddComponent<MeshComponent>(eCube2, new MeshComponent(2, 0));
-    m_ECS.AddComponent<TransformComponent>(eCube2, new TransformComponent());
-    m_ECS.GetComponent<TransformComponent>(eCube2)->position = { -0.4, 0, 0 };*/
-   /* m_ECS.GetComponent<TransformComponent>(eSquare)->position = { 0, 0.5, 2 };*/ // IL FAUT FAIRE UNE FONTION QUI APPEL TJRS dirty = true APRES CHAQUE MODIFICATION, SINON LE TRANSFORM EST PAS UPDATE
-
+   
     int i = 0;
     // Message et boucle de rendu
     MSG msg = {};
--- LyonPlex/src/LyonPlexLib/SampleScene.cpp
+++ LyonPlex/src/LyonPlexLib/SampleScene.cpp
@@ -4,15 +4,51 @@
 void SampleScene::Start()
 {
 	OutputDebugStringA("\nHI ! I am sample scene. \n");
+    
+   Entity camEntity = mp_EcsManager->CreateEntity();
+   mp_EcsManager->AddComponent<CameraComponent>(camEntity, new CameraComponent());
 
-	Entity eTriangle = mp_EcsManager->CreateEntity();
+
+    Entity eSquare = mp_EcsManager->CreateEntity();
+   mp_EcsManager->AddComponent<MeshComponent>(eSquare, new MeshComponent(1, 0));
+   mp_EcsManager->AddComponent<TransformComponent>(eSquare, new TransformComponent());
+   mp_EcsManager->GetComponent<TransformComponent>(eSquare)->position = { -0.60, 0.4, 0.2 };
+   mp_EcsManager->GetComponent<TransformComponent>(eSquare)->scale = { 2, 2, 1 };
+
+    /*Entity eSquare2 =mp_EcsManager->CreateEntity();
+   mp_EcsManager->AddComponent<MeshComponent>(eSquare2, new MeshComponent(1, 0));
+   mp_EcsManager->AddComponent<TransformComponent>(eSquare2, new TransformComponent());
+   mp_EcsManager->GetComponent<TransformComponent>(eSquare2)->position = { -0.20, 0.4, 0.2 };*/
+
+    Entity eTriangle = mp_EcsManager->CreateEntity();
+   mp_EcsManager->AddComponent<MeshComponent>(eTriangle, new MeshComponent(0, 0));
+   mp_EcsManager->AddComponent<TransformComponent>(eTriangle, new TransformComponent());
+   mp_EcsManager->GetComponent<TransformComponent>(eTriangle)->position = { 0.60, 0.4, 0.2 };
+   mp_EcsManager->GetComponent<TransformComponent>(eTriangle)->scale = { 3, 3, 1 };
+    /*Entity eTriangle2 =mp_EcsManager->CreateEntity();
+   mp_EcsManager->AddComponent<MeshComponent>(eTriangle2, new MeshComponent(0, 0));
+   mp_EcsManager->AddComponent<TransformComponent>(eTriangle2, new TransformComponent());
+   mp_EcsManager->GetComponent<TransformComponent>(eTriangle2)->position = { -0.20, -0.4, 0.2 };*/
+
+    Entity eCube = mp_EcsManager->CreateEntity();
+   mp_EcsManager->AddComponent<MeshComponent>(eCube, new MeshComponent(2, 0));
+   mp_EcsManager->AddComponent<TransformComponent>(eCube, new TransformComponent());
+   mp_EcsManager->GetComponent<TransformComponent>(eCube)->position = { 0.2, -0.6, 0 };
+
+    /*Entity eCube2 =mp_EcsManager->CreateEntity();
+   mp_EcsManager->AddComponent<MeshComponent>(eCube2, new MeshComponent(2, 0));
+   mp_EcsManager->AddComponent<TransformComponent>(eCube2, new TransformComponent());
+   mp_EcsManager->GetComponent<TransformComponent>(eCube2)->position = { -0.4, 0, 0 };*/
+    /*mp_EcsManager->GetComponent<TransformComponent>(eSquare)->position = { 0, 0.5, 2 };*/ // IL FAUT FAIRE UNE FONTION QUI APPEL TJRS dirty = true APRES CHAQUE MODIFICATION, SINON LE TRANSFORM EST PAS UPDATE
+
+	/*Entity eTriangle = mp_EcsManager->CreateEntity();
 	mp_EcsManager->AddComponent<MeshComponent>(eTriangle, new MeshComponent(0, 0));
 
 	Entity eSquare = mp_EcsManager->CreateEntity();
 	mp_EcsManager->AddComponent<MeshComponent>(eSquare, new MeshComponent(1, 0));
 
 	Entity eCube = mp_EcsManager->CreateEntity();
-	mp_EcsManager->AddComponent<MeshComponent>(eCube, new MeshComponent(2, 0));
+	mp_EcsManager->AddComponent<MeshComponent>(eCube, new MeshComponent(2, 0));*/
 }
 
 void SampleScene::Update()

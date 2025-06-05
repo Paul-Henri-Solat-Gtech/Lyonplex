d9b02647d79a8a6741f06e5a7e42e99e9f9ecf47
Paulhenrisolat <psolat@gaming.tech>
2025-06-05 20:36:04 +0200

    Update main.cpp

--- LyonPlex/src/SampleProject/main.cpp
+++ LyonPlex/src/SampleProject/main.cpp
@@ -1,10 +1,14 @@
 ﻿#include "pch.h"
 #include "main.h"
 
+#include "SampleScene2.h"
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int) 
 {
     GameManager game(hInstance);
     if (!game.Init())
         return -1;
     return game.Run();
+
+
 }
